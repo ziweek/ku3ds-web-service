e89e86e84177fa98759e768a592e25f738d90530
Austin <austin.jiuk.kim@gmail.com>
2022-10-29 04:02:34 +0900

    code opti

--- frontend/components/main/content-launch.js
+++ frontend/components/main/content-launch.js
@@ -4,21 +4,21 @@ import AnimationEarth from "../animation/animation-planet-earth";
 export default function ContentLaunch() {
   return (
     <>
-      <section class="text-gray-600 body-font">
-        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          {/* <img class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"> */}
+      <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+          {/* <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600"> */}
           <AnimationRocket />
-          <div class="text-center lg:w-2/3 w-full">
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">
+          <div className="text-center lg:w-2/3 w-full">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
               Microdosing synth tattooed vexillologist
             </h1>
-            <p class="mb-8 leading-relaxed">
+            <p className="mb-8 leading-relaxed">
               같으며, 되려니와, 작고 낙원을 노래하며 이것이야말로 인간은
               인도하겠다는 말이다. 이상 이것을 얼마나 우리는 역사를 충분히 길지
               때까지 사막이다. 하는 든 아니한 사막이다.
             </p>
-            <div class="flex justify-center">
-              {/* <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"> */}
+            <div className="flex justify-center">
+              {/* <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"> */}
               {/* Button
               </button> */}
             </div>
--- frontend/components/main/hero.js
+++ frontend/components/main/hero.js
@@ -3,21 +3,21 @@ import AnimationSatelite from "../animation/animation-satelite";
 export default function Hero() {
   return (
     <>
-      <section class="text-gray-500 body-font">
-        <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-          <div class="text-center lg:w-2/3 w-full">
+      <section className="text-gray-500 body-font">
+        <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+          <div className="text-center lg:w-2/3 w-full">
             <AnimationSatelite />
-            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium">
               Microdosing synth tattooed vexillologist
             </h1>
-            <p class="mb-8 leading-relaxed">
+            <p className="mb-8 leading-relaxed">
               이런 오면 슬퍼하는 청춘이 지나고 있습니다. 한 이름과, 나는 쉬이
               많은 강아지, 아스라히 가을로 멀리 거외다. 시인의 소학교 멀리 딴은
               듯합니다. 다 덮어 아침이 봅니다. 프랑시스 나는 별 하나에 때
               있습니다. 비둘기, 못 남은 하나에 이런 북간도에 까닭입니다. 새워
               풀이 걱정도 하나에 까닭입니다.
             </p>
-            <div class="flex justify-center"></div>
+            <div className="flex justify-center"></div>
           </div>
         </div>
       </section>
