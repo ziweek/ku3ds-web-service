295c24a82fcd6dd736ba264cdc10920c32596382
Austin <austin.jiuk.kim@gmail.com>
2022-11-06 14:36:08 +0900

    Add Fullscreen

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,6 +5,15 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
+      <style global jsx>{`
+        html,
+        body,
+        body > div:first-child,
+        div#__next,
+        div#__next > div {
+          height: 100%;
+        }
+      `}</style>
     </>
   );
 }
