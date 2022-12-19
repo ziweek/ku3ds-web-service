c4525b026971d68a5066515c08915f2671a0fa5d
alex <alex.jiuk.kim@gmail.com>
2022-12-19 23:55:10 +0900

    Add .gitignore

--- web-edu/frontend/components/layout.js
+++
@@ -1,7 +0,0 @@
-export default function Layout({ children }) {
-  return (
-    <>
-      <div className="flex items-center justify-center">{children}</div>
-    </>
-  );
-}
--- web-edu/frontend/pages/_app.js
+++
@@ -1,8 +0,0 @@
-import "../styles/globals.css";
-import "../public/static/fonts/style.css";
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
-
-export default MyApp;
